ba354225709ce79d981ab3395950c355f022c74a
sumitZ98 <136557487+sumitZ98@users.noreply.github.com>
2024-05-07 10:32:04 +0000

    commited

--- stringMethods.py
+++ stringMethods.py
@@ -1,3 +1,4 @@
+import re
 # Concatenation in string
 
 Fname = "Sumit"
@@ -25,3 +26,55 @@ print("modified text:", newText);
 text = "today is a very good day to go out"
 words = text.split()
 print("Words:", words);
+
+#string strip
+
+text = "      there are some spaces      "
+newText = text.strip()
+print("New text:", newText)
+
+# Sub string 
+
+text = "python is a very cool language"
+substring = "cool"
+if substring in text:
+    print(substring,"found in the text")
+
+#other than normal +,-,*,// there is rounding off
+
+num1 = 1.2547893
+res = round(num1,3)
+print(res);
+
+#absolute value
+
+num = -25 
+res = abs(num)
+print(res)
+
+# Regex match and search
+text = "The quick brown fox"
+pattern = r"fox"
+# search = re.match(pattern, text)
+search = re.search(pattern,text)
+if search:
+    print("pattern found", search.group())
+else: 
+    print("pattern not found")
+
+# replace using regex
+
+text = "The quick brown fox"
+pattern = r"brown"
+replacement = "green"
+
+newText = re.sub(pattern, replacement, text)
+print(newText)
+
+#split
+
+text = "a,b,c,d,e"
+pattern = r","
+
+res = re.split(pattern, text)
+print(res)
