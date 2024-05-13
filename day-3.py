128e26258d40ed40b4ed49ce0379652828f8db50
sumitZ98 <136557487+sumitZ98@users.noreply.github.com>
2024-05-13 12:02:38 +0000

    cmit

---
+++ day-3.py
@@ -0,0 +1,14 @@
+# KEYWORDS **given in git hub repo 26 imp keywords
+a=10
+b=5
+
+def addition():
+    a=20
+    b=30
+    print(a+b)
+
+def subtraction():
+    print(a-b)
+
+addition()
+subtraction()
---
+++ day4.py
@@ -0,0 +1,20 @@
+
+
+
+def Add(a,b):
+    return (a+b)
+
+def Sub(a,b):
+    return (a-b)
+    
+def Mul(a,b):
+    return (a*b)
+
+def Div(a,b):
+    return (a/b)
+
+
+a=2
+b=3
+c = Add(a,b)
+print (c)
