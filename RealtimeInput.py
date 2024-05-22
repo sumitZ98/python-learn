e1072a3f437a40548c560119d6b3e144fbaf4159
sumitZ98 <136557487+sumitZ98@users.noreply.github.com>
2024-05-22 10:48:01 +0000

    new changes

---
+++ RealtimeInput.py
@@ -0,0 +1,20 @@
+import os
+
+folders = input("please provide list of folder names with spaces in between").split()
+
+for folder in folders:
+
+    try:
+        files=os.listdir(folder)        #searching for folder in the directories given in the input
+    except FileNotFoundError:
+        print("Please provide a valid folder name")
+        continue
+        
+    print("listing files for the folder" + folder)
+
+    # print(files)
+
+    for file in files:
+        print(file)
+
+# exception hnadling 
---
+++ loops.py
@@ -0,0 +1,14 @@
+# fruits = [
+#     "INFO: Operation successful",
+#    "ERROR: File not found",
+#    "DEBUG: Connection established",
+#    "ERROR: Database connection failed",
+#    ]
+
+# for fruit in fruits:
+#     if "ERROR" in fruit:
+#         print(fruit)
+
+
+a = input("ahehao aojwoahd awdad").split()
+print(a)
